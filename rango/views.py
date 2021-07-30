828dbf0dadef5022e0dda2eb3ed130e4b05511a0
Tianjiao-ZHANG <2588833Z@student.gla.ac.uk>
2021-07-30 14:12:44 +0100

    Chapter 10 exercises completed

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
 
 
@@ -26,14 +27,32 @@ def index(request):
     context_dict['mostviewed'] = 'Most Viewed Pages'
     context_dict['pages'] = page_list
 
+    visitor_cookie_handler(request)
     return render(request, 'rango/index.html', context=context_dict)
+    # context_dict['visits'] = request.session['visits']
+
+    # context_dict['visits'] = int(request.COOKIES.get('visits', '1'))#
+
+    # response = render(request, 'rango/index.html', context=context_dict)
+    # visitor_cookie_handler(request, response)
+    # return response##
+
+    # request.session.set_test_cookie()
+
+    # return render(request, 'rango/index.html', context=context_dict)#
     # return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 def about(request):
-    print(request.method)
-    print(request.user)
+    # print(request.method)
+    # print(request.user)
 
-    return render(request, 'rango/about.html')
+    # if request.session.test_cookie_worked():
+    #     print("TEST COOKIE WORKED!")
+    #     request.session.delete_test_cookie()
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
 def show_category(requset, category_name_slug):
@@ -92,7 +111,6 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context=context_dict)
 
 def register(request):
-    
     registered = False
     if request.method == 'POST':
         user_form = UserForm(request.POST)
@@ -145,6 +163,36 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    # visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+
+    # last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+        # response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        request.session['last_visit'] = last_visit_cookie
+        # response.set_cookie('last_visit', last_visit_cookie)
+    request.session['visits'] = visits
+    # response.set_cookie('visits', visits)
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+
+
 
 
 
